aef0e6bb7a3648f77e8131aa9c5accefa059ad03
Joy6000 <fnafgamer545@gmail.com>
2021-03-12 12:27:02 -0800

    Added Kick Prefab

---
+++ commands/kick.js
@@ -0,0 +1,12 @@
+const { execute } = require("./purge");
+
+module.exports = {
+    name: 'kick',
+    reqArgs: 1,
+    userPerms: 'KICK_MEMBERS',
+    botPerms: 'KICK_MEMBERS',
+    usage: '<Target ID> <Reason>',
+    async execute({ message, args, handler, client }) {
+        
+    }
+}
